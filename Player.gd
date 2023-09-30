ecb97ba82a5829b7a434536f044ba6d49180a29a
Saskapult <thekieransmith@gmail.com>
2023-09-30 11:00:55 -0700

    goblin and crate

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$GoblinSprite.play()
+	else:
+		$GoblinSprite.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
