153439f9f69160ccf9dd46bb76b00d065938aa25
Rowan Althorp <rowalthorp@gmail.com>
2023-10-01 03:11:43 -0600

    This is practically the end product of the movement and my stuff... yay

--- Scene/main.gd
+++ Scene/main.gd
@@ -1,8 +1,30 @@
 extends Node2D
 
-@onready var timer = $Timer
+var bullet = preload("res://Bullet.tscn")
+var remove_track = 1
 
-@export var bullet: PackedScene
+func _ready(): 
+	var bnum = BulletNum.num_bullet
+	for i in bnum :
+		var instance = bullet.instantiate()
+		add_child(instance)
 
-
+	
+func _process(delta):
+		
+		if LivesManager.lives > 0 and LivesManager.lives_counter == 0:
+			remove_track += 1
+			
+			_removal()
+			LivesManager.lives_counter = 1
+			_ready()
+			
+func _removal():
+	var instances_to_remove = 2 * remove_track
+	var children_count = get_child_count()
+	
+	for i in range(children_count-1, children_count - instances_to_remove - 1, -1):
+		var instance = get_child(i)
+		instance.queue_free()
+	
 	
