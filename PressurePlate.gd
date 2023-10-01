9b93a19a994ac0468bb0d12fd767f7cfebb7f46a
Saskapult <thekieransmith@gmail.com>
2023-09-30 18:10:18 -0700

    pressure plate release

--- PressurePlate.gd
+++ PressurePlate.gd
@@ -1,6 +1,8 @@
 extends Area2D
 signal hit
 
+var body_count = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +13,17 @@ func _ready():
 func _process(delta):
 	pass
 
+
 func _on_body_entered(body):
 	#hide() # Player disappears after being hit.
 	#hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	#$CollisionShape2D.set_deferred("disabled", true)
+	body_count += 1
 	$AnimatedSprite2D.frame = 1
 
+
+func _on_body_exited(body):
+	body_count -= 1
+	if body_count == 0:
+		$AnimatedSprite2D.frame = 0
