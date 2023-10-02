d6e043a9de24afa2e63cf61897b1defe3e571130
Saskapult <thekieransmith@gmail.com>
2023-10-02 13:57:25 -0700

    levels 3-*

---
+++ Levels/3-1.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $PressurePlate.pressed:
+		$ExitDoor.open = true
+	else:
+		$ExitDoor.open = false
+		
+	if $ExitDoor.open && $ExitDoor.get_overlapping_bodies().find($Player) != -1:
+		print("Level complete, change scene")
+		get_tree().change_scene_to_file("res://Levels/3-2.tscn")
