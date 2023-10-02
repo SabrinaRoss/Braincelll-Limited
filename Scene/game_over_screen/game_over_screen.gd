4b4e70a7ab97bb2133c89f09d5fa3411d86b4a44
Saskapult <thekieransmith@gmail.com>
2023-10-02 11:08:28 -0700

    game over main menu button

--- Scene/game_over_screen/game_over_screen.gd
+++ Scene/game_over_screen/game_over_screen.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @export var restart_level = "res://Scene/level_0.tscn"
+@export var menu_level = "res://Scene/start_screen/start_scene.tscn"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,3 +17,7 @@ func _process(delta):
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file(restart_level)
 	
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file(menu_level)
