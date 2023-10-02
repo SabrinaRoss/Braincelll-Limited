9dccd9eccb1c81b20c414cc1b87b7b663e20168e
James Tang <rhsjamestang2019@gmail.com>
2023-10-02 00:33:27 -0700

    Popup container completed
    - made a workaround with using "visibility" of the canvas layer
    - removed the pop_container because it was causing mouse input problem

--- Scene/popup/PopUp.gd
+++ Scene/popup/PopUp.gd
@@ -4,25 +4,25 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start()
-	print($Popup.is_visible())
+	hide_qte()
+	
+
+func show_qte():
+	$popup_container.visible = true
+
+func hide_qte():
+	$popup_container.visible = false
+
+
 
 func _on_timer_timeout():
 	if($Timer.is_stopped()):
 		print("It's about to pop off!!")
+		show_qte()
 		
-		$Popup.show()
-		print($Popup.is_visible())
 		
 	
 	
 
 
 
-func _on_popup_mouse_entered():
-	print("Yes this mouse has enter the scene")
-	
-
-
-
-func _on_popup_mouse_exited():
-	print("This mouse has left the business")
